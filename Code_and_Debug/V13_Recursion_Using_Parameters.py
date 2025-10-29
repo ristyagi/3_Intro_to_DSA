a87941bfb3ff412671cd05f7080a6c7ba9d8cb69
Rishabh Tyagi <30928799+ristyagi@users.noreply.github.com>
2025-10-29 23:16:35 +0530

    Update V13_Recursion_Using_Parameters.py

--- Code_and_Debug/V13_Recursion_Using_Parameters.py
+++ Code_and_Debug/V13_Recursion_Using_Parameters.py
@@ -16,10 +16,29 @@ DSA Python Course 2025 - Recursion Using Parameters - Part 13 [Hindi] | Code & D
 
 ##### print x, n times using recursion #####
 
-def print_func(x, n):
+##### print 1 to n head recursion with two parameters #####
+def func_print_n_two_param_head(i, n):
+    if i>n:
+        return
+    print(i)                            # head recursion as action takes place before fn call
+    func_print_n_two_param_head(i+1, n)
+func_print_n_two_param_head(1,5)
+
+
+##### print n to 1 tail recursion with two parameters #####
+def func_print_n_two_param_tail(i, n):
+    if i>n:
+        return
+    func_print_n_two_param_tail(i+1, n)
+    print(i)                            # tail recursion as action takes place before fn call
+func_print_n_two_param_tail(1,5)
+
+
+
+##### print n to 1 head recursion with one parameter #####
+def print_func(n):
     if n == 0:
         return
-    print(x)
-    print_func(x, n-1) # recursion fn call, so that next iteration is called with n-1
-
-print_func(15,4)
+    print(n)
+    print_func(n-1) # recursion fn call, so that next iteration is called with n-1
+print_func(5)
