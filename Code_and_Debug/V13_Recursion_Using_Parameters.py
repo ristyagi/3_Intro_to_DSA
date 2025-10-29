c949e8ffbda552888b97ec4fa04cb826405a6d22
Rishabh Tyagi <30928799+ristyagi@users.noreply.github.com>
2025-10-29 15:13:03 +0530

    Create V13_Recursion_Using_Parameters.py

---
+++ Code_and_Debug/V13_Recursion_Using_Parameters.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  29 22:12:27 2025
+
+@author: Rishabh_Tyagi
+"""
+
+"""
+https://www.youtube.com/watch?v=fgurDxhawRw&list=PLhR2IpV1b2FwWwviBHRrR118YAaSlyhTU&index=15
+
+DSA Python Course 2025 - Recursion Using Parameters - Part 13 [Hindi] | Code & Debug
+"""
+
+
+##### print x, n times using recursion #####
+
+def print_func(x, n):
+    if n == 0:
+        return
+    print(x)
+    print_func(x, n-1) # recursion fn call, so that next iteration is called with n-1
+
+print_func(15,4)
